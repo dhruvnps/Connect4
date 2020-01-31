8c52602045fc36af22bd1da3e87c7cd5cadd37a5
dhruvpattem <dhruv.pattem@gmail.com>
2020-01-31 21:09:14 +0000

    Formatting

--- Connect4_Basic.py
+++ Connect4_Basic.py
@@ -36,7 +36,8 @@ def draw_board(win):
                 color = RED
             elif BOARD[row][column] == 2:
                 color = YELLOW
-            pygame.draw.circle(win, color, (int((column + 0.5) * GRID_SIZE), int((ROW_LEN - (row - 0.5)) * GRID_SIZE)), RADIUS)
+            pygame.draw.circle(win, color, (int((column + 0.5) * GRID_SIZE),
+                                            int((ROW_LEN - (row - 0.5)) * GRID_SIZE)), RADIUS)
 
 
 def draw_mouse(win, mouse_x, turn):
@@ -68,51 +69,51 @@ def win_check():
             if y + 3 < ROW_LEN and x + 3 < COLUMN_LEN:
                 d2 = [BOARD[y + i][x + i] for i in range(4)]
 
-            if ([1 for i in range(4)] in (row, column, d1, d2)) or ([2 for i in range(4)] in (row, column, d1, d2)):
+            if ([1, 1, 1, 1] in (row, column, d1, d2)) or [2, 2, 2, 2] in (row, column, d1, d2)):
                 return True
     return False
 
 
 def main():
-    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    win=pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
-    turn = 1
-    first_refresh = True
+    turn=1
+    first_refresh=True
 
-    running = True
+    running=True
     while running:
         draw_board(win)
 
         if first_refresh:
-            first_refresh = False
+            first_refresh=False
             pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                running=False
 
             if event.type == pygame.MOUSEMOTION:
-                mouse_x = event.pos[0]
+                mouse_x=event.pos[0]
                 draw_mouse(win, mouse_x, turn)
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x = event.pos[0]
-                column = int(math.floor(mouse_x / GRID_SIZE))
+                mouse_x=event.pos[0]
+                column=int(math.floor(mouse_x / GRID_SIZE))
 
                 if turn == 1:
                     drop_coin(column, turn)
-                    turn = 2
+                    turn=2
 
                 else:
                     drop_coin(column, turn)
-                    turn = 1
+                    turn=1
 
                 draw_board(win)
                 draw_mouse(win, mouse_x, turn)
                 pygame.display.update()
 
                 if win_check():
-                    running = False
+                    running=False
 
     print(np.flipud(BOARD))
     pygame.time.wait(10000)
